185c5ea27167dd12166821cda5df856a0d86c03b
Saad Aslam <saadusufzai@gmail.com>
2020-07-18 14:47:20 +0500

    cloud animation added

--- src/Animation.js
+++ src/Animation.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import classes from './animation.module.css'
 import clouds from './images/cloud.png'
+
+import useWebAnimations from "@wellyshen/use-web-animations";
 const Animation = () => {
+    const { ref, playState } = useWebAnimations({
+        keyframes: {
+          transform: ["translateX(-40px)",
+                      "translateX(1600px)",
+                    ],
+        },
+        timing: {
+            duration:10000,
+            iterations:Infinity
+        }
+
+    });
+    const { ref2, playState2 } = useWebAnimations({
+        keyframes: {
+          transform: ["translateX(-40px)",
+                      "translateX(1600px)",
+                    ],
+        },
+        timing: {
+            duration:10000,
+            iterations:Infinity
+        }
+
+    });
+
     return (
         <div className={classes.bg}>
             <div className={classes.clouds}>
 
-                <img  className={classes.cloud1} src={clouds}/>
-                <img  className={classes.cloud2} src={clouds}/>
+                <img ref={ref}  className={classes.cloud1} src={clouds}/>
+                <img ref={ref2}  className={classes.cloud2} src={clouds}/>
                 <img  className={classes.cloud3} src={clouds}/>
                 <img  className={classes.cloud4} src={clouds}/>
             </div>
