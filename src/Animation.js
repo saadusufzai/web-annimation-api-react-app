a12a150274bc50b325f9fe8cc403eb3603393c92
Saad Aslam <saadusufzai@gmail.com>
2020-07-18 12:21:19 +0500

    background added

--- src/Animation.js
+++ src/Animation.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import classes from './animation.module.css'
+import clouds from './images/cloud.png'
 const Animation = () => {
     return (
         <div className={classes.bg}>
+            <div className={classes.clouds}>
+
+                <img  className={classes.cloud1} src={clouds}/>
+                <img  className={classes.cloud2} src={clouds}/>
+                <img  className={classes.cloud3} src={clouds}/>
+                <img  className={classes.cloud4} src={clouds}/>
+            </div>
             <h1>ANIMAITON</h1>
 
         </div>
