3f0fb72939b7fe49f0ef2e64736adec382621869
Saad Aslam <saadusufzai@gmail.com>
2020-07-18 11:43:00 +0500

    initials set

---
+++ src/Animation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './animation.module.css'
+const Animation = () => {
+    return (
+        <div className={classes.bg}>
+            <h1>ANIMAITON</h1>
+
+        </div>
+    )
+}
+
+export default Animation
