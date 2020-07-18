3bc8b906bb248585e06643c1d84d16112e0462d9
Saad Aslam <saadusufzai@gmail.com>
2020-07-18 20:52:35 +0500

    box and cloud updated

--- src/Animation.js
+++ src/Animation.js
@@ -9,19 +9,19 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 const Animation = () => {
     const { ref:cloud1, playState } = useWebAnimations({
         keyframes: [
-            { transform: "translateX(1800px)" },
-            { transform: "translateX(-1500px)" },
+            { transform: "translateX(700%)" },
+            { transform: "translateX(-100%)" },
           ],
         timing: {
-            duration:25000,
+            duration:20000,
             iterations:Infinity
         }
 
     });
     const { ref:cloud2, playState2 } = useWebAnimations({
         keyframes: [
-            { transform: "translateX(1500px)" },
-            { transform: "translateX(-1500px)" },
+            { transform: "translateX(500%)" },
+            { transform: "translateX(-100%)" },
           ],
         
         timing: {
@@ -33,8 +33,8 @@ const Animation = () => {
     
     const { ref:cloud3, playState3 } = useWebAnimations({
         keyframes: [
-            { transform: "translateX(1500px)" },
-            { transform: "translateX(-1500px)" },
+            { transform: "translateX(600%)" },
+            { transform: "translateX(-100%)" },
           ],
         
         timing: {
@@ -47,8 +47,8 @@ const Animation = () => {
     
     const { ref:cloud4, playState4 } = useWebAnimations({
         keyframes: [
-            { transform: "translateX(1500px)" },
-            { transform: "translateX(-1500px)" },
+            { transform: "translateX(100%)" },
+            { transform: "translateX(-100%)" },
           ],
         
         timing: {
@@ -90,20 +90,8 @@ const Animation = () => {
         }
 
     });
-    const { ref:mountain3, playState7 } = useWebAnimations({
-        keyframes: [
-            { transform: "translateX(1500px)" },
-            { transform: "translateX(-1500px)" },
-                        
-          ],
-        
-        timing: {
-            
-            duration:45000,
-            iterations:Infinity
-        }
-
-    });
+   
+   
     const { ref:mountain0, playState8 } = useWebAnimations({
         keyframes: [
             
@@ -120,7 +108,7 @@ const Animation = () => {
 
     });
 
-    const { ref:queen, playState10 } = useWebAnimations({
+    const { ref:queen, runner ,getAnimation } = useWebAnimations({
         keyframes: [
             { transform: "translateY(0)" },
             { transform: "translateY(-100%)" },
@@ -130,17 +118,22 @@ const Animation = () => {
         timing: {
             easing: 'steps(7, end)',
             duration:1200,
-            iterations:Infinity
+            iterations:Infinity,
+            playbackRate:200000
         }
 
     });
+
+    const speed = ()=>{
+        console.log( + 'animation');
+    }
 
 
 
     return (
         
         <div  className={classes.bg}>
-            <div className={classes.box}>
+            <div onClick={()=>speed()} className={classes.box}>
             <img ref={queen} className={classes.bandi} src={bandi}/>
             </div>
             <img src={mountain} className={classes.mountain} ref={mountain0} />
@@ -148,14 +141,14 @@ const Animation = () => {
            
            
             <div className={classes.clouds}>
-            <div  >
+            
                 <img ref={cloud1} className={classes.cloud1} src={clouds}/>
                 <img ref={cloud2} className={classes.cloud2} src={clouds}/>
                 <img ref={cloud3} className={classes.cloud3} src={clouds}/>
                 <img ref={cloud4} className={classes.cloud4} src={clouds}/>
+            
             </div>
-            </div>
-            <h1>ANIMAITON</h1>
+           
             <img src={mountain} className={classes.mountain} ref={mountain1} />
             <img src={mountain} className={classes.mountain1} ref={mountain2} />
 
