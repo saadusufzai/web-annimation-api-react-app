bd399f99f32af8f0e6e334f98f5bc41c5f4d9f72
Saad Aslam <saadusufzai@gmail.com>
2020-07-20 16:20:25 +0500

    build errors eliminated

--- src/fighter.js
+++ src/fighter.js
@@ -1,42 +1,34 @@
-import React from 'react'
-import figher from './images/3.png'
-import classes from './fighter.module.css'
-import useWebAnimaiton from '@wellyshen/use-web-animations'
+import React from "react";
+import figher from "./images/3.png";
+import classes from "./fighter.module.css";
+import useWebAnimaiton from "@wellyshen/use-web-animations";
 
-const Fighter = (figherSpeed) => {
+const Fighter = () => {
+  const { ref: fighterAni } = useWebAnimaiton({
+    keyframes: [
+      { transform: "translateX(0)" },
+      { transform: "translateX(-100%)" },
+    ],
+    timing: {
+      easing: "steps(6,end)",
+      duration: 1000,
+      iterations: Infinity,
+      playbackRate: 1,
+    },
+  });
 
-    const {ref:fighterAni , getAnimation:f1} = useWebAnimaiton({
+  return (
+    <div>
+      <div className={classes.box}>
+        <img
+          ref={fighterAni}
+          className={classes.fighter}
+          src={figher}
+          alt={"fighter"}
+        />
+      </div>
+    </div>
+  );
+};
 
-        keyframes:[
-            {transform: 'translateX(0)'},
-            {transform: 'translateX(-100%)'},
-        ],
-        timing:{
-            easing:'steps(6,end)',
-            duration:1000,
-            iterations:Infinity,
-            playbackRate:1
-        }
-
-
-    })
-    var frames=[f1]
-    
-    const speed = ()=>{
-        console.log('click')
-   }
-
-  const speedDown = ()=> {
-   frames.forEach((e)=>e().updatePlaybackRate(e().playbackRate/1.3))
-   }
-
-    return (
-        <div>
-            <div   className={classes.box}>
-                <img onClick={()=>speed} ref={fighterAni} className={classes.fighter} src={figher} />
-            </div>
-        </div>
-    )
-}
-
-export default Fighter
+export default Fighter;
